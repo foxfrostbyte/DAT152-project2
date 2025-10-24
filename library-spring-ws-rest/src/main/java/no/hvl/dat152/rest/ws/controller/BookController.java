1da71fe6381ed9cac7a5af199466e5a190efa9ad
Elena N.J. <elena.nicole.jacobsen@hotmail.com>
2025-10-24 19:36:20 +0200

    Cleanup BookController + BookService
    Removal of unnecessary lines. All tests passed without them.

--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/controller/BookController.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/controller/BookController.java
@@ -1,18 +1,10 @@
-/**
- * 
- */
 package no.hvl.dat152.rest.ws.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,17 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import no.hvl.dat152.rest.ws.exceptions.BookNotFoundException;
-import no.hvl.dat152.rest.ws.exceptions.UpdateBookFailedException;
 import no.hvl.dat152.rest.ws.model.Author;
 import no.hvl.dat152.rest.ws.model.Book;
 import no.hvl.dat152.rest.ws.service.BookService;
 
 /**
- * @author tdoy
+ * REST API controller for managing books
  */
 @RestController
 @RequestMapping("/elibrary/api/v1")
--- library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/service/BookService.java
+++ library-spring-ws-rest/src/main/java/no/hvl/dat152/rest/ws/service/BookService.java
@@ -1,24 +1,18 @@
-/**
- * 
- */
 package no.hvl.dat152.rest.ws.service;
 
 import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import no.hvl.dat152.rest.ws.exceptions.BookNotFoundException;
-import no.hvl.dat152.rest.ws.exceptions.UpdateBookFailedException;
 import no.hvl.dat152.rest.ws.model.Author;
 import no.hvl.dat152.rest.ws.model.Book;
 import no.hvl.dat152.rest.ws.repository.BookRepository;
 
 /**
- * @author tdoy
+ * Service layer for managing books
  */
 @Service
 public class BookService {
@@ -28,27 +22,19 @@ public class BookService {
 
 
 	public Book saveBook(Book book) {
-		
 		return bookRepository.save(book);
-		
 	}
 	
 	public List<Book> findAll(){
-		
 		return (List<Book>) bookRepository.findAll();
-		
 	}
 	
 	
 	public Book findByISBN(String isbn) throws BookNotFoundException {
-
-		Book book = bookRepository.findByIsbn(isbn)
-				.orElseThrow(() -> new BookNotFoundException("Book with isbn = "+isbn+" not found!"));
-
-		return book;
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new BookNotFoundException("Book with isbn = "+isbn+" not found!"));
 	}
-	
-	// TODO public Book updateBook(Book book, String isbn)
+
 	public Book updateBook(Book book, String isbn) throws BookNotFoundException {
 		Book b = bookRepository.findByIsbn(isbn)
 				.orElseThrow(() -> new BookNotFoundException("Book with isbn = "+isbn+" not found!"));
@@ -58,23 +44,16 @@ public class BookService {
 
 		return b;
 	}
-	
-	// TODO public List<Book> findAllPaginate(Pageable page)
 
-	// TODO public Set<Author> findAuthorsOfBookByISBN(String isbn)
 	public Set<Author> findAuthorsOfBookByISBN(String isbn) throws BookNotFoundException {
 		Book book = bookRepository.findByIsbn(isbn)
 				.orElseThrow(() -> new BookNotFoundException("Book with isbn = "+isbn+" not found!"));
 		return book.getAuthors();
 	}
-	
-	// TODO public void deleteById(long id)
-	
-	// TODO public void deleteByISBN(String isbn)
+
 	public void deleteByISBN(String isbn) throws BookNotFoundException {
 		Book b = bookRepository.findByIsbn(isbn)
 				.orElseThrow(() -> new BookNotFoundException("Book with isbn = "+isbn+" not found!"));
-
 		bookRepository.delete(b);
 	}
 	
